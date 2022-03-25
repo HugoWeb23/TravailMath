567863fac74e366d6121c913b5e844bc71da67ce
HugoWeb23 <hugohourriez@live.be>
2022-03-25 21:29:05 +0100

    Exercice 2

--- Exercice_2.py
+++ Exercice_2.py
@@ -4,7 +4,7 @@ def isprime(num):
             return False
     return True
 
-type = int(input("Que voulez-vous faire ?\nCrypter un message => 1\nDécrypter un message => 2"))
+type = int(input("Que voulez-vous faire ?\nCrypter un message => 1\nDécrypter un message => 2\n=> "))
 
 if type == 1:
 
@@ -12,24 +12,24 @@ if type == 1:
     print(f"Le code ASCII de la lettre est : {ord(m)}")
     m = ord(m)
 
-    p = int(input("Veuillez définir P (nombre premier)"))
+    p = int(input("Veuillez définir P (nombre premier) : "))
     while isprime(p) == False:
         print(f"Le chiffre {p} n'est pas un nombre premier")
-        p = int(input("Veuillez définir P (nombre premier)"))
+        p = int(input("Veuillez définir P (nombre premier) : "))
 
-    q = int(input("Veuillez définir Q (nombre premier)"))
+    q = int(input("Veuillez définir Q (nombre premier) : "))
     while isprime(q) == False:
         print(f"Le chiffre {q} n'est pas un nombre premier")
-        q = int(input("Veuillez définir Q (nombre premier)"))
+        q = int(input("Veuillez définir Q (nombre premier) : "))
 
     while p == q:
         print("P et Q ne peuvent pas être identiques")
-        q = int(input("Veuillez définir Q (nombre premier)"))
+        q = int(input("Veuillez définir Q (nombre premier) : "))
 
-    e = int(input(f"Veuillez définir E (nombre premier et plus grand que {p} et {q})"))
+    e = int(input(f"Veuillez définir E (nombre premier) : "))
     while isprime(e) == False:
         print(f"Le chiffre {e} n'est pas un nombre premier")
-        e = int(input(f"Veuillez définir E (nombre premier)"))
+        e = int(input(f"Veuillez définir E (nombre premier) : "))
 
     r = p * q
     z = (p - 1) * (q - 1)
@@ -38,19 +38,21 @@ if type == 1:
     n = 0
     while d.is_integer() == False:
         d = ((z * n) + 1) / e
+        print(f"Recherche du nombre entier (({z} * {n}) + 1) / {e}) = {((z * n) + 1) / e}")
         n+=1
 
-    print(f"Clé publique : {e}, {r}")
+    print(f"Calcul de la clé d'encryptage (({z} * {n - 1}) + 1) / {e}) = {d}")
+
+    print(f"Clé publique (P * Q) => ({p} * {q}) : {r}")
     print(f"Clé privée : {d}")
 
     C = m**e % r
 
-    print(f"Message crypté : {C}")
+    print(f"Calcul du message crypté C ({m}^{e} modulo {r}): {C}")
 
 if type == 2:
     MessageCrypte = int(input("Veuillez saisir le message crypté : "))
     d = int(input("Veuillez saisir la clé privée : "))
     r = int(input("Veuillez saisir la clé publique : "))
     Message = MessageCrypte**d % r
-    print(d, r)
-    print(f"Message décrypté : {Message}")
+    print(f"Message décrypté ({MessageCrypte}^{d} modulo {r}) : {Message}")
