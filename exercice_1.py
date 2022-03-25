9747e10ec4beb157d939f9433e4458cd555b54b0
HugoWeb23 <hugohourriez@live.be>
2022-03-25 12:18:56 +0100

    Exercice 1

---
+++ exercice_1.py
@@ -0,0 +1,30 @@
+alphabet = ['T', 'A', 'D', 'U', 'J', 'V', 'X', 'C', 'G', 'W', 'M', 'Y', 'E', 'S', 'H', 'K', 'N', 'P', 'B', 'Q', 'R', 'L', 'Z', 'F', 'O', 'I', ' ']
+lettres = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' ']
+
+print("Que souhaitez-vous faire ?\nChiffrer un message => 1\nDéchiffrer un message => 2")
+type = int(input("Veuillez saisir le numéro de l'action à effectuer : "))
+
+if type == 1:
+    message = input("Veuillez saisir le message à chiffrer : ")
+    chiffrage = str()
+    for lettre in message:
+        if lettre == ' ':
+            chiffrage += alphabet[6]
+        elif lettre == 'G':
+            chiffrage += alphabet[26]
+        else:
+            chiffrage += alphabet[ord(lettre) - 65]
+    print(f"Message chiffré : {chiffrage}")
+
+if type == 2:
+    message = input("Veuillez saisir le message à déchiffrer : ")
+    decryptage = str()
+    for lettre in message:
+        if lettre == ' ':
+            decryptage += lettres[6]
+        elif lettre == 'X':
+            decryptage += lettres[26]
+        else:
+            decryptage += lettres[alphabet.index(lettre)]
+    print(f"Message déchiffré: {decryptage}")
+
---
+++ exercice_8_2.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+A=np.array([[1,2,0],[4,3,-1]])
+B=np.array([[5,1],[2,3],[3,4]])
+
+result = np.dot(A,B)
+
+print(result)
